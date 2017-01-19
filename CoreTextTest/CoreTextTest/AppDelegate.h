f544dd0017335fdb35e3f85dc077aab26fda2e68
jiuxiaoming <jiuxiaoming@jiuxiaomingdeMacBook-Air.local>
2017-01-19 11:47:24 +0800

    新工程初创
    新建空工程

---
+++ CoreTextTest/CoreTextTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreTextTest
+//
+//  Created by jiuxiaoming on 17/1/19.
+//  Copyright © 2017年 jiuxiaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
